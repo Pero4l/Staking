354362a236852a86cdc01bcbd8f898a7b0d3d7ab
pero4l <lilpero03@gmail.com>
2026-06-08 14:43:26 +0100

    verified

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -29,6 +29,16 @@ module.exports = {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
     },
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=11155111",
+          browserURL: "https://sepolia.etherscan.io",
+        },
+      },
+    ],
   },
   sourcify: {
     enabled: true
